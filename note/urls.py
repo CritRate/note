1022eea14e1254d3135e57deb1867e8dc84d02fe
CritRate <Miro.Hud@gmail.com>
2021-01-27 15:55:40 +0000

    basic GET,UPDATE,DELETE,POST views for note

--- note/urls.py
+++ note/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 from django.urls import reverse
 
-name = 'note'
+from .views import CreateNote, UpdateNote, DeleteNote, ListViewNote
+
+app_name = 'note'
 
 urlpatterns = [
-    path('', RedirectView.as_view(url=reverse('note:home'))),
-    path('note', TemplateView.as_view(template_name='note/home.html'), name='home')
+    path('', TemplateView.as_view(template_name='note/home.html'), name='home'),
+    path('notes', ListViewNote.as_view(), name='list_view' ),
+    path('new/note', CreateNote.as_view(), name='create_note'),
+    path('delete/note/<pk>', DeleteNote.as_view(), name='delete_note'),
+    path('update/note/<pk>', UpdateNote.as_view(), name='update_note'),
 ]
--- note/views.py
+++ note/views.py
@@ -1,3 +1,27 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, ListView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
+from .models import Note
+
+class CreateNote(CreateView):
+    model = Note
+    fields = ['title', 'content']
+    success_url = reverse_lazy('note:list_view')
+
+
+class UpdateNote(UpdateView):
+    model = Note
+    fields = ['title', 'content']
+    success_url = reverse_lazy('note:list_view')
+
+class DeleteNote(DeleteView):
+    model = Note
+    success_url = reverse_lazy('note:list_view')
+
+
+class ListViewNote(ListView):
+    model = Note
+    paginate_by = 10
