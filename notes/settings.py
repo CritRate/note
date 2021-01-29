a02aab9b1c81bbdfbff1b180e4e95dd043130063
CritRate <Miro.Hud@gmail.com>
2021-01-29 20:38:21 +0000

    added static for css and js, staticfiles_dir and basic grid for app

--- notes/settings.py
+++ notes/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGOUT_REDIRECT_URL = 'note:home'
 LOGIN_REDIRECT_URL = 'note:home'
-
 
 # CONTEXT PROCESSOR to get user notes available to all templates
 TEMPLATES[0]['OPTIONS']['context_processors'].append('note.context_processors.my_notes')
